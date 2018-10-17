ef511a56e4d2368bd1818057317eefb132b01084
Olivia Johnson <oliviajohnson@Olivias-MacBook-Pro-2.local>
2018-10-17 18:39:38 -0500

    pinkparty fun thats correct

--- js/filters/pinkparty.js
+++ js/filters/pinkparty.js
@@ -3,14 +3,24 @@ function pinkparty(){
     loadPixels() // load all pixel data into the "pixels" array
     // loop as many times as there are pixel values in the pixels array
     for( let i=0; i<pixels.length; i+=4){
-        let r = pixels[i] // red value
-        let g = pixels[i+1] // green value
-        let b = pixels[i+2] // blue value
         pixels[i] = 255 // keeps red value full
-        pixels[i+1] = Math.floor(Math.random() * 255)// adjusts green to keep pink
-        pixels[i+2] = Math.floor(Math.random() * 156 + 99)// adjusts blue to keep pink
+        pixels[i+1] = map(i,0,pixels.length,0,255)// adjusts green to keep pink
+        pixels[i+2] = map(i,0,pixels.length, 99,255)// adjusts blue to keep pink
     }
-    updatePixels() // update canvas from modified "pixels" array
-    updatePixels() // update canvas from modified "pixels" array
+    /*for( let i=0; i<pixels.length; i+=4){
+      if( Math.random()<0.3 ) {
+        pixels[i] = 255
+        pixels[i+1] = 255// adjusts green to keep pink
+        pixels[i+2] = 255// adjusts blue to keep pink
+      }
+    }*/
+  updatePixels() // update canvas from modified "pixels" array
+    for( let i=0; i<10000; i++ ){
+      let x = Math.random()*width
+      let y= Math.random()*height
+      fill(255)
+      noStroke()
+      rect(x,y,2,2)
+    }
     pop() // restore original drawing settings
 }
