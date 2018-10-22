0ca61436882fac5e75ea13ef55a5eec588a842fc
jose-carlos-pena <jpena3@saic.edu>
2018-10-22 15:07:44 -0500

    Made a webcam option

--- js/filters/webcam.js
+++ js/filters/webcam.js
@@ -1,9 +1,23 @@
 var cam
 
-function webcam(){
-    cam = createCapture(VIDEO)
-    cam.size(innerHeight,innerWidth)
+function webcamSetup(){
 
-    cam.position(mouseX,0);
-    image(capture,0,0,innerHeight,innerWidth);
+  cam = createCapture(IMAGE)
+  cam.size(innerHeight,innerWidth)
+
+}
+
+function webcam(){
+
+    push()
+    if(!cam){
+      console.log("hi")
+      cam = createCapture(VIDEO)
+      cam.size(innerHeight,innerWidth)
+      cam.hide()
+    }
+
+    image(cam,0,0,innerHeight,innerWidth);
+    pop()
+
 }
