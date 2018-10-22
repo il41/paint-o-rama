41afac4e59c08b6b0736acb6483792bec71507e6
homingdevice <mconlo@saic.edu>
2018-10-22 12:06:59 -0500

    added opensource+extra simple annotations to code and final image of code for background. resized erase rectangle brush

--- js/brushes/erase.js
+++ js/brushes/erase.js
@@ -1,21 +1,23 @@
+ // ~*~*~*~*~~*~*~*~*~~*~*~*~*~hi mom~*~*~*~*~~*~*~*~*~~*~*~*~*~~*~*~*~*~
+// <*><*><*><*><*><*><*><*><*><*><*><*><*><*><*><*><*><*><*><*><*><*><*>
 // ~*~*~*~*~~*~*~*~*~~*~*~*~*~hi mom~*~*~*~*~~*~*~*~*~~*~*~*~*~~*~*~*~*~
 
 
-let ctx
-function erase(x,y){
-    push() // save current drawing settings
-    fill(100,100,20)
-    ctx.clearRect(x,y,50,50)
+let ctx   //declares a variable and names it ctx (context) to globally (meaning throughout the  entire code) call     later
+function erase(x,y){  // name the function, set up two variables for the x and y axis
+    push() // save current drawing settings by adding to the end of  the array (meaning the list of )
+    fill(0,0,0)  // sets fill of eraser brush that will be removed in the  next line
+    ctx.clearRect(x,y,100,100)   // calls function of eraser brush sets  dimensions to 100 pixels squared
     pop() // restore original drawing settings
 }
 
-function eraseSetUp(drawingContext){
-    ctx=drawingContext
-    push()
-    fill(255,255,255)
-    noStroke()
-    rect(0,0,width, height)
-    document.body.style.backgroundImage="url(../images/backgroundCode.png)"
-    pop()
+function eraseSetUp(drawingContext){  // creates a new function called eraseSetUp and defines the variable (input for the function) as drawingContext This will set up a white background to be drawn on, and then creates a background image to be revealed when the erase brush is used
+    ctx=drawingContext  //the global variable ctx is called and set to the drawingContext
+    push()  //save current drawing settings  by  adding to the end of the array (meaning the list of )
+    fill(255,255,255)  //sets the primary background color to white
+    noStroke()  //removes border color around white background
+    rect(0,0,width, height)  //sets dimensions of white background  to the browser width and height
+    document.body.style.backgroundImage="url(../images/backgroundCodeMidterm.png)"     //calls the backgroundImage (screenshot of   this code!)
+    pop() // restore original drawing settings
 
 }
