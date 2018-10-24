5b7b114cb5e264601a450286745f387995329184
sflato <stefancvitanic@gmail.com>
2018-10-23 20:47:14 -0500

    formatted code and added my icon into the images folder

--- js/brushes/acidhouseBrush.js
+++ js/brushes/acidhouseBrush.js
@@ -5,11 +5,18 @@ function loadacidhouseImages(){
     let acidhouseURLs = [
         'acidhouseimages/dogcoins.png',
         'acidhouseimages/peace.png',
-        'acidhouseimages/smiley-clipart-house-3.png'
+        'acidhouseimages/smiley-clipart-house-3.png',
+        'acidhouseimages/smiley-1.png',
+        'acidhouseimages/smiley-2.png',
+        'acidhouseimages/simley-4.png',
+        'acidhouseimages/smiley-5.png',
+
+
     ]
+
     // loop as many times as there are paths in the array
     for (let i = 0; i < acidhouseURLs.length; i++) {
-        // load the next cat image, assign it to the image variable
+        // load the next image, assign it to the image variable
         let img = loadImage( acidhouseURLs[i] )
         // add that img data to the acidhouse array
         acidhouse.push( img )
@@ -17,21 +24,28 @@ function loadacidhouseImages(){
 }
 
 function acidhouseBrush(x,y){
+
     push() // save current drawing settings
+
     colorMode(HSB)// sets the color mode to HSB
+
     // random numbers for hsb values between 0 and 255
     let value1 = random( 255 )
     let value2 = random( 255 )
     let value3 = random( 255 )
-    let randomsize = random( 75 )
-    // the operation that determines the color of the rectangle
+
+    // random numbers for the size (width) of the rectangle
+    let randomsize = random( 40,70 )
+
+    // the operation that applies the color of the rectangle
     fill( frameCount % 360, value1,value2 )
     stroke(value1,value2,value3)
+
     //draws a rectangle behind the image
-    rect( x, y, randomsize, 50)
+    rect( x, y, randomsize,randomsize)
 
     let ran = random( 0, acidhouse.length-1 ) // pick a random index from the acidhouse array
     ran = floor( ran ) // make sure it is not a float by rounding down
-    image( acidhouse[ran], x, y, randomsize, 50) // draw a random image from the
+    image( acidhouse[ran], x, y, randomsize, randomsize) // draw a random image from the
     pop() // restore original drawing settings
 }
