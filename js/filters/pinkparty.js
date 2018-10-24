1fe8592418893806dd8834b81151b9f177845755
Olivia Johnson <oliviajohnson@Olivias-MacBook-Pro-2.local>
2018-10-24 08:59:49 -0500

    woo hoo added comment

--- js/filters/pinkparty.js
+++ js/filters/pinkparty.js
@@ -17,7 +17,5 @@ function pinkparty(){
   updatePixels() // update canvas from modified "pixels" array
   pop() // restore original drawing settings
 }
-<<<<<<< HEAD
 // yee haw!
-=======
->>>>>>> 7ec4040f14f1046bf1f8818b051b5faa41ecb985
+// woo hoo!
